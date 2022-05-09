256303215029a4ec394247f75591a52dac13635a
Carlos Mario <carlosmario7lg@gmail.com>
2022-05-09 17:56:34 -0500

    adelantar animacion  carrusel

--- JS/Animaciones.js
+++ JS/Animaciones.js
@@ -1,24 +1,63 @@
-//Animacion imagen 1//
-var animacionActual = 1
 
-let animation = setInterval(() => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Animacion imagen 1//
+// let imagen1 = document.querySelector(".imagen1")
+// let imagen2 = document.querySelector(".imagen2")
+// let imagen3 = document.querySelector(".imagen3")
+// let imagen4 = document.querySelector(".imagen4")
+
+// var animacionActual = 1
+
+// let animation = setInterval(() => {
   
-  switch(animacionActual) {
-    case 1: 
-      imagen1.style.setProperty("animation-play-state", "running")
-      imagen4.style.setProperty("animation-play-state", "paused")
-      animacionActual++;
-      break;
-    
-    case 2: 
-      imagen1.style.setProperty("animation-play-state", "running")
-      imagen4.style.setProperty("animation-play-state", "paused")
-      animacionActual++;
-      break;
-  }
+//   switch(animacionActual) {
+//     case 1: 
+//       imagen1.style.setProperty("animation-play-state", "running")
+//       imagen4.style.setProperty("animation-play-state", "paused")
+//       animacionActual++;
+//       break;
+//     case 2: 
+//       imagen1.style.setProperty("animation-play-state", "paused")
+//       imagen2.style.setProperty("animation-play-state", "running")
+//       animacionActual++;
+//       break;
+//     case 3: 
+//       imagen2.style.setProperty("animation-play-state", "paused")
+//       imagen3.style.setProperty("animation-play-state", "running")
+//       animacionActual++;
+//       break;
+//     case 4: 
+//       imagen3.style.setProperty("animation-play-state", "paused")
+//       imagen4.style.setProperty("animation-play-state", "running")
+//       animacionActual = 1
+//       break;  
+
+//   }
   
-  console.log(`${animationState}`)
-}, 5000);
+//   console.log(`${animacionActual}`)
+// }, 5000);
 
 
 
