afac066c8c5d219ee193927e481a2400de89c388
jorge <geolg763@gmail.com>
2022-05-12 18:09:11 -0500

    add type delete efect

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,13 @@ const loading = document.getElementById("loading");
 const parrafo = document.getElementById("parrafo");
 const logo = document.getElementById("logo");
 const content = document.getElementById("content");
+const content2 = document.getElementById("content2");
 const footer = document.getElementById("footer");
 const audio = document.getElementById("audio");
+const descripcion = document.getElementById("descripcion");
+const bntNext = document.getElementById('bnt-next')
+const section = document.getElementById('section')
+
 
 const text =
   "The year is 3055." +
@@ -13,28 +18,28 @@ const text =
 
 var i = 0;
 
-function dealay(n){
-    return new Promise(function(resolve){
-        setTimeout(resolve, n*1000)
-    })
+const text2 = 
+
+function dealay(n) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, n * 1000);
+  });
 }
 
-async function typeWriter() {
+async function typeWriter(time) {
   if (i < text.length) {
     if (text[i] == ".") {
-        audio.muted = true;
-        parrafo.innerHTML += text[i];
-        await dealay(.8)
-        parrafo.innerHTML += "<br><br>";
+      audio.muted = true;
+      parrafo.innerHTML += text[i];
+      await dealay(0.8);
+      parrafo.innerHTML += "<br><br>";
     } else {
-        audio.muted = false;
-        parrafo.innerHTML += text[i];
+      audio.muted = false;
+      parrafo.innerHTML += text[i];
     }
     i++;
-    setTimeout(typeWriter, 70);
+    setTimeout(typeWriter, time);
   } else {
-    
-    loading.style.opacity = "1";
     footer.style.opacity = "1";
   }
 }
@@ -42,9 +47,8 @@ async function typeWriter() {
 function opacidad() {
   logo.style.opacity = ".2";
   content.style.display = "flex";
-  
 
-  typeWriter();
+  typeWriter(10 , 'parrafo');
 }
 
 window.addEventListener("load", () => {
@@ -53,3 +57,60 @@ window.addEventListener("load", () => {
 
   setTimeout(opacidad, 2000);
 });
+
+function borrar(parrafo){
+
+    let element = parrafo.firstElementChild
+
+    function typeDelete(){
+    
+        if(parrafo.innerHTML !== ''){
+    
+            let last = element.innerHTML.length - 1
+    
+            element.innerHTML = element.innerHTML.substring( 0, last)
+    
+            setTimeout(typeDelete, 1)
+    
+        }else{
+
+            audio.muted = true;
+            element.classList.remove('visible')
+
+        }
+
+    }
+
+    typeDelete()
+
+}
+
+
+bntNext.addEventListener('click', () => {
+
+    for(let i = 0 ; i < section.children.length; i++){
+        
+        if(section.children[i].classList[1] === 'visible'){
+
+            borrar(section.children[i])
+
+            
+
+            if(section.children[i].previousElementSibling.tagName == 'DIV'){
+                console.log(1)
+                section.children[i].previousElementSibling.classList.add('visible')
+                break
+            }
+
+            if(section.children[i].nextElementSibling.tagName == 'DIV'){
+                console.log(1)
+                section.children[i].nextElementSibling.classList.add('visible')
+                break
+            }
+
+        }
+    }
+
+})
+
+
